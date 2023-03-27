67250b36719707e85eeac5542e9ab7d5d7149abd
24HourDeveloper <latoya30310@gmail.com>
2023-03-27 14:34:24 -0400

    added js code for image changes

---
+++ index.js
@@ -0,0 +1,18 @@
+const calendar = document.getElementById('calendar')
+const mainImg = document.getElementById('main-img')
+const main = document.getElementById('main')
+const devices = document.getElementById('devices')
+const contract = document.getElementById('contract')
+
+calendar.addEventListener('click',() => changeImg('./img/calendar-svgrepo-com (1).png'))
+
+devices.addEventListener('click',() => changeImg('./img/devices-svgrepo-com.png'))
+
+contract.addEventListener('click',() => changeImg('./img/open-book-book-svgrepo-com.png'))
+
+main.addEventListener('click',() => changeImg('./img/fitness-removebg-preview.png'))
+
+function changeImg(img) {
+    mainImg.src = img
+    console.log('image');
+}
